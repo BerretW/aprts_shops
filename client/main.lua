8be08f3bf89a0eb5dc749de6500cb6a9649935ae
Berretw <berretw01@gmail.com>
2025-12-26 10:49:43 +0100

    refactor: Improve shop zone and blip management with enhanced logic and cleanup

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,9 @@
 local CreatedShops = {}
-
-local CreatedShops = {}
 local CreatedBlips = {} -- Pole pro blipy
 
 -- Funkce pro vytvoření/aktualizaci Blipu
 function UpdateShopBlip(shopId, data)
-    -- Pokud blip existuje, smažeme ho
+    -- Pokud blip existuje, smažeme ho, abychom ho vytvořili znovu (aktualizace)
     if CreatedBlips[shopId] then
         RemoveBlip(CreatedBlips[shopId])
     end
@@ -29,6 +27,7 @@ function UpdateShopBlip(shopId, data)
 end
 
 function CreateShopZone(shopData)
+    -- Pokud zóna existuje, smažeme ji (pro refresh)
     if CreatedShops[shopData.shop_id] then exports.ox_target:removeZone(CreatedShops[shopData.shop_id]) end
 
     -- Vytvoříme/Aktualizujeme Blip
@@ -48,13 +47,13 @@ function CreateShopZone(shopData)
                     local open = shopData.settings and shopData.settings.openHour or 0
                     local close = shopData.settings and shopData.settings.closeHour or 24
                     
-                    -- Jednoduchá logika: Pokud je open < close (např 8-20)
-                    -- Pokud je open > close (např 22-06), logika je složitější, ale pro začátek:
+                    -- Logika otevírací doby
                     local isOpen = false
                     if open < close then
+                        -- Např. 08:00 - 20:00
                         isOpen = (currentHour >= open and currentHour < close)
                     else
-                        -- Přes půlnoc
+                        -- Přes půlnoc (např. 22:00 - 06:00)
                         isOpen = (currentHour >= open or currentHour < close)
                     end
 
@@ -85,55 +84,13 @@ RegisterNetEvent('aprts_shops:client:refreshShopData', function(shopData)
     CreateShopZone(shopData)
 end)
 
--- Načtení
-CreateThread(function()
-    Wait(1000) 
-    lib.callback('aprts_shops:getShopData', false, function(shops) 
-        if shops then
-            for k, data in pairs(shops) do
-                CreateShopZone(data)
-            end
-        end
-    end)
-end)
-function CreateShopZone(shopData)
-    if CreatedShops[shopData.shop_id] then exports.ox_target:removeZone(CreatedShops[shopData.shop_id]) end
-
-    local zoneId = exports.ox_target:addSphereZone({
-        coords = vec3(shopData.coords.x, shopData.coords.y, shopData.coords.z),
-        radius = 1.0,
-        options = {
-            {
-                name = 'open_shop_ui',
-                icon = 'fa-solid fa-basket-shopping',
-                label = 'Nakupovat',
-                onSelect = function()
-                    -- ZMĚNA: Otevíráme naše vlastní NUI místo ox_inventory
-                    OpenBuyerNUI(shopData.shop_id)
-                end
-            },
-            {
-                name = 'manage_shop',
-                icon = 'fa-solid fa-crown',
-                label = 'Spravovat obchod',
-                onSelect = function()
-                    OpenManagementNUI(shopData.shop_id)
-                end,
-                canInteract = function() return true end 
-            }
-        }
-    })
-    CreatedShops[shopData.shop_id] = zoneId
-end
-
 -- Nová funkce pro otevření menu pro ZÁKAZNÍKA
 function OpenBuyerNUI(shopId)
     -- Získáme čerstvá data o obchodu
     lib.callback('aprts_shops:getShopData', false, function(shop)
         if not shop then return end
         
-        -- Musíme získat labely itemů (názvy), protože v DB máme jen spawn name
-        -- Použijeme ox_inventory export
+        -- Získání labelů itemů
         local itemsWithLabels = {}
         if shop.products then
             for item, data in pairs(shop.products) do
@@ -151,10 +108,28 @@ function OpenBuyerNUI(shopId)
 
         SetNuiFocus(true, true)
         SendNUIMessage({
-            action = 'openBuyer', -- Jiná akce než pro admina
+            action = 'openBuyer',
             shopLabel = shop.label,
             shopId = shop.shop_id,
             items = itemsWithLabels
+        })
+    end, shopId)
+end
+
+-- Funkce pro otevření menu pro MAJITELE
+function OpenManagementNUI(shopId)
+    lib.callback('aprts_shops:openManagement', false, function(data)
+        if not data then 
+            lib.notify({type = 'error', description = 'Nemáš přístup k tomuto obchodu.'})
+            return 
+        end
+        
+        SetNuiFocus(true, true)
+        SendNUIMessage({
+            action = 'open',
+            shopData = data.shopData,
+            inventoryItems = data.inventory,
+            isAdmin = data.isAdmin -- ZDE BYLA CHYBA (bylo tam false)
         })
     end, shopId)
 end
@@ -164,8 +139,6 @@ RegisterNUICallback('buyItem', function(data, cb)
     TriggerServerEvent('aprts_shops:server:buyItem', data)
     cb('ok')
 end)
-
--- ... (Zbytek kódu, OpenManagementNUI atd. zůstává stejný)
 
 -- Synchronizace nového obchodu (když admin vytvoří)
 RegisterNetEvent('aprts_shops:client:syncNewShop', function(data)
@@ -178,11 +151,16 @@ RegisterNetEvent('aprts_shops:client:removeShopZone', function(shopId)
         exports.ox_target:removeZone(CreatedShops[shopId])
         CreatedShops[shopId] = nil
     end
+    -- Smažeme i blip
+    if CreatedBlips[shopId] then
+        RemoveBlip(CreatedBlips[shopId])
+        CreatedBlips[shopId] = nil
+    end
 end)
 
--- Načtení při startu
+-- Hlavní načtení při startu (smazal jsem ten druhý duplicitní Thread)
 CreateThread(function()
-    Wait(1000)
+    Wait(1000) 
     lib.callback('aprts_shops:getShopData', false, function(shops) 
         if shops then
             for k, data in pairs(shops) do
@@ -191,23 +169,6 @@ CreateThread(function()
         end
     end)
 end)
-
-function OpenManagementNUI(shopId)
-    lib.callback('aprts_shops:openManagement', false, function(data)
-        if not data then 
-            lib.notify({type = 'error', description = 'Nemáš přístup k tomuto obchodu.'})
-            return 
-        end
-        -- print(json.encode(data))
-        SetNuiFocus(true, true)
-        SendNUIMessage({
-            action = 'open',
-            shopData = data.shopData,
-            inventoryItems = data.inventory,
-            isAdmin = data.isAdmin
-        })
-    end, shopId)
-end
 
 -- NUI Callbacks
 RegisterNUICallback('close', function(data, cb)
